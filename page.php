818d3e680952862705760e7e08e64f08008529d1
yusufmuhammad-311910617 <81587959+yusufmuhammad-311910617@users.noreply.github.com>
2021-06-15 13:34:04 +0700

    Add files via upload

---
+++ page.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace  App\Controllers;
+
+class  Page  extends  BaseController
+{
+public  function  about()
+{
+echo "Ini halaman About";
+}
+
+public  function  contact()
+{
+echo "Ini halaman Contact";
+}
+
+public  function  faqs()
+{
+echo  "Ini  halaman  FAQ";
+}
+}
